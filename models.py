6c56b448737bdcaa9b7471a92733900145f4cd32
Erik Klem <mrerikk6@gmail.com>
2023-06-22 12:56:56 -0400

    Added default False for admin

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    admin = db.Column(db.Boolean)
+    admin = db.Column(db.Boolean, default=False)
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
